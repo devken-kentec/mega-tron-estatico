2b573fc695b6f2d5dea6636317eb71f2e4b6aff4
devken-kentec <kennedy_tomazete@outlook.com>
2025-09-27 22:24:54 -0300

    site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { SharedService } from './shared/shared.service';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -14,6 +14,7 @@ export class AppComponent {
   title = 'MegaTron';
 
   private sharedService = inject(SharedService);
+  private router = inject(Router);
 
   public open: boolean = false;
   public anoCorrente!: number;
@@ -21,12 +22,17 @@ export class AppComponent {
   ngOnInit() {
     this.anoCorrente = new Date().getFullYear();
     this.pegarTokenRequisicao();
+    this.router.navigate(['/home']);
     //console.log(this.sharedService.getTokenRequisicao());
   }
-
 
   public pegarTokenRequisicao(): void {
     let tokens = this.sharedService.token[Math.floor(Math.random() * this.sharedService.token.length)];
     this.sharedService.setTokenRequisicao(tokens);
+  }
+
+  public abrirMenu() {
+    this.open = !this.open;
+
   }
 }
